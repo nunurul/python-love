08cc9dd2d948ced8144ff290d2be14eb6ddf40b0
nunurul <noe.rul.khoir@gmail.com>
2024-11-10 16:48:16 +0700

    menambahkan folder

---
+++ 1. Introduction-python/Main.py
@@ -0,0 +1,18 @@
+import time
+start_time = time.time()
+
+print("Hello world")
+print("Halo Dunia")
+
+print("Hai jelek")
+
+a = 10
+
+print(a) # ini comment
+# halo ini comment
+
+""" hai katanya ini comment """
+
+for i in range(1, 1000):
+    a=10
+print(time.time() - start_time, "detik")

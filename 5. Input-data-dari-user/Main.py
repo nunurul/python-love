ccf6f737c59c0e45aee6278ae5efbc1642670e46
nunurul <noe.rul.khoir@gmail.com>
2024-11-12 13:23:14 +0700

    user-input

---
+++ 5. Input-data-dari-user/Main.py
@@ -0,0 +1,19 @@
+# str
+
+str = input("Masukan str :")
+print("ini adalah : ", str, "dengan type : ", type(str))
+
+# int
+
+into = input("masukan angka : ")
+print("ini angka : ", into, "dengan type : ", type(int(into)))
+
+# float
+
+floating = float(input("masukan angka : "))
+print("ini angka : ", floating, "ini angka", type(floating))
+
+# booleam
+
+biner = bool(int(input("masukan angka : ")))
+print("ini angka : ", biner , "dengan type data : ", type(biner))

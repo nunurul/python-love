f6499c6417ff4bc4d8fc1accdecd5cc2a7ed1932
nunurul <noe.rul.khoir@gmail.com>
2024-11-18 14:59:32 +0700

    konversi suhu

---
+++ 7.konversi-suhu/Main.py
@@ -0,0 +1,21 @@
+# konversi suhu
+
+# celcius 
+
+celcius = float(input("Masukan Suhu : "))
+print("suhu celcius nya adalah", celcius, "celcius")
+
+# reamur 
+
+reamur = 4/5 * celcius
+print("suhu reamur nya adalah", reamur, "reamur")
+
+# fahrenheit
+
+fahrenheit = 9/5 * celcius + 32
+print("suhu fahrenheitnya adalah", fahrenheit, "fahrenheit")
+
+# kelvin
+
+kelvin = celcius + 273 
+print("suhu kelvinnya adalah", kelvin, "kelvin")
